c13a19ac7f9ffe8ef94618e95a51525971c383f9
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-06-19 13:22:16 +0200

    hw-11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,63 +1,30 @@
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import 'modern-normalize/modern-normalize.css';
-// import imageUrl from '../img/alert-icon.svg';
-// import resolveImageUrl from '../img/resolve-icon.svg';
+export { galleryItems };
 
-// const inputForm = document.querySelector('.form');
-// inputForm.addEventListener('submit', userPromise);
-
-// function userPromise(e) {
-//   e.preventDefault();
-
-//   const delay = e.target.delay.value;
-//   const status = e.target.state.value;
-
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (status === 'fulfilled') {
-//         resolve(delay);
-//       } else {
-//         reject(delay);
-//       }
-//     }, delay);
-//   });
-
-//   promise
-//     .then(delay => {
-//       iziToast.success({
-//         title: 'OK',
-//         message: `Fulfilled promise in ${delay}ms`,
-//         messageSize: '16',
-//         messageColor: '#fff',
-//         backgroundColor: '#59a10d',
-//         position: 'topRight',
-//         close: true,
-//         closeOnEscape: true,
-//         closeOnClick: true,
-//         progressBar: true,
-//         progressBarColor: '#326101',
-//         iconColor: '#fff',
-//         iconUrl: resolveImageUrl,
-//         theme: 'dark',
-//       });
-//     })
-//     .catch(delay => {
-//       iziToast.error({
-//         title: 'Error',
-//         message: `Rejected promise in ${delay}ms`,
-//         messageSize: '16',
-//         messageColor: '#fff',
-//         backgroundColor: '#ef4040',
-//         position: 'topRight',
-//         close: true,
-//         closeOnEscape: true,
-//         closeOnClick: true,
-//         progressBar: true,
-//         progressBarColor: '#ffbebe',
-//         iconUrl: imageUrl,
-//         theme: 'dark',
-//       });
-//     });
-// }
-// import * as hits from './pixabay-api';
+function galleryItems(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <a class="gallery-item" href="${largeImageURL}" >
+        <img
+          src="${webformatURL}"
+          alt="${tags}" loading='lazy height= 30vh'
+        />
+      <div class="info">
+      <p class="info-item"><b>likes:</b>"${likes}"</p>
+      <p class="info-item"><b>Views:</b>"${views}"</p>
+      <p class="info-item"><b>Comments:</b>"${comments}"</p>
+      <p class="info-item"><b>Downloads:</b>"${downloads}"</p>
+      </div></a> `;
+      }
+    )
+    .join(' ');
+}
